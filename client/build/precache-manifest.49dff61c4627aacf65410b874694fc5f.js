63aae255fb82cd0f69c48c8faf3d43c97523fd75
[eschwelgin] <[eschwelgin@gmail.com]>
2020-04-28 21:00:18 -0400

    deploy again built

---
+++ client/build/precache-manifest.49dff61c4627aacf65410b874694fc5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9968e8d1c8f51d85c808",
+    "url": "/static/css/main.b06356bb.chunk.css"
+  },
+  {
+    "revision": "9968e8d1c8f51d85c808",
+    "url": "/static/js/main.d35f9836.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad636674bcb06987b68d",
+    "url": "/static/css/2.9a5ce8e9.chunk.css"
+  },
+  {
+    "revision": "ad636674bcb06987b68d",
+    "url": "/static/js/2.8c65c905.chunk.js"
+  },
+  {
+    "revision": "3ae4114ad7c0dd8f884bf3e87552923c",
+    "url": "/static/media/logo2.3ae4114a.png"
+  },
+  {
+    "revision": "c481eac3f6d6500260490077c61eaffe",
+    "url": "/static/media/white-logo.c481eac3.png"
+  },
+  {
+    "revision": "5f62897f307662bb432e0922d8fcef8d",
+    "url": "/index.html"
+  }
+];
